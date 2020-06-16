fe01bab8b51593be0af119e3a6c8fc00471197e0
Akash09-ak <55475437+Akash09-ak@users.noreply.github.com>
2020-06-17 01:12:12 +0530

    Add files via upload

---
+++ BinaryGap.py
@@ -0,0 +1,21 @@
+def solution(N):
+   b=bin(N)
+   l=list(b)
+   print (b)
+   l = l[2:]
+   c = 0
+   d = 0
+   for i in range(0, len(l)):
+
+       if l[i] == '1':
+           if(c>d):
+               d=c
+           c=0
+
+       else:
+           c=c+1
+
+   print(d)
+   return l
+
+print(solution(32))
